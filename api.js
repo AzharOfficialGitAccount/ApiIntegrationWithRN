30ccc3eba3a333edfba47643deed1cf57cc4453c
AzharOfficial <azharofficial599@gmail.com>
2021-02-22 16:58:03 +0530

    seventh commit

---
+++ api.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {FlatList, View, Text} from 'react-native';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+  async callApi() {
+    let response = await fetch(
+      'https://facebook.github.io/react-native/movies.json',
+    );
+    let responseJson = await response.json();
+    this.setState({data: responseJson.movies});
+  }
+
+  render() {
+    return (
+      <View>
+        <Text
+          style={{
+            fontSize: 40,
+            margin: 10,
+            textAlign: 'center',
+          }}>
+          Api Integration Basic Example{' '}
+        </Text>
+        <FlatList
+          data={this.state.data}
+          renderItem={({item}) => (
+            <Text
+              style={{fontSize: 25, margin: 10, backgroundColor: 'skyblue'}}>
+              {item.id}, {item.title}, {item.releaseYear}
+            </Text>
+          )}
+        />
+      </View>
+    );
+  }
+}
+export default App;
